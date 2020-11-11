73ffb8f28e0ec8b026f217d5714cbcb714dab36b
RebelDroid09 <paulcs91@gmail.com>
2020-11-10 22:35:06 -0600

    Update app.py
    Added database call and reflection.

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite://Resources/hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+measurements = Base.classes.measurement
+stations = Base.classes.station
 
 app = Flask(__name__)
 
